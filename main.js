aa29f20f67ca620879bd81a9f2dd599e7ac11f81
sebastian <sebasr1268@gmail.com>
2023-03-13 19:19:07 -0500

    last commit

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const divnew=document.querySelector('.agregado')
 const pendientes=document.querySelector('.pendientes')
-cont=0
-pendientes.innerHTML=`tareas realizadas ${cont} y tareas pendientes ${cont}`
+let cont=0
+let cont2=0
+pendientes.innerHTML=`Tareas realizadas ${cont2} y tareas pendientes ${cont}`
+
 agregar.addEventListener('click',()=>{
     const divnew2 =document.createElement('div')
     const eliminar2=document.createElement('button')
+  
     eliminar2.className='eliminar'
     eliminar2.style.backgroundImage='url(./imagenes/basura.svg)'
     divnew2.style.backgroundColor='white'
@@ -13,12 +16,16 @@ agregar.addEventListener('click',()=>{
     divnew.append(divnew2)
     divnew2.append(eliminar2)
     cont++
+    pendientes.innerHTML=`Tareas realizadas ${cont2} y tareas pendientes ${cont}`
+
 
 })
 agregado.addEventListener('click',(event)=>{
     if(event.target.className=='eliminar'){
         const papa = event.target.parentElement
         agregado.removeChild(papa)
+        cont2++
         cont--
+        pendientes.innerHTML=`Tareas realizadas ${cont2} y tareas pendientes ${cont}`
     }
 })
